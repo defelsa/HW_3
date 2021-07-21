8600ae973a94f47670c3a85938cf8249c838a7f3
Yelyzaveta Verbovetska <86962463+defelsa@users.noreply.github.com>
2021-07-21 23:32:02 +0300

    script_2.py

--- Task_3/script_2.py
+++ Task_3/script_2.py
@@ -1,19 +1,17 @@
 import math 
 from math import sqrt 
- 
-def hypotenuse(a,b): 
-   if a != 0 and b != 0: 
-            hypo = func(a, b) 
-            print("При катетах {} та {}, гіпотенуза дорівнює {}".format(a, b, hypotenuse)) 
-   else: 
-     print('Довжина катета не може = 0') 
-     return hypotenuse 
-   
-@decor_ 
-def hypotenuse(a,b): 
+
+def decor_(f):
+    def wrapper(a, b):
+        hypotenuse = f(a, b)
+        print(f"Катеты {a} {b} гипотенуза {hypotenuse}")
+    return wrapper
+
+@decor_
+def hypotenuse(a, b): 
   if a != 0 and b != 0: 
     return math.sqrt(a**2 + b**2) 
   else: 
      print('Довжина катета не може = 0') 
- 
-hypotenuse_(6, 7)
+
+hypotenuse(3, 4)
