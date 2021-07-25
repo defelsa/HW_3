5da39978f4460c8afe876932fddb8130c17ef15f
Yelyzaveta Verbovetska <86962463+defelsa@users.noreply.github.com>
2021-07-25 22:29:18 +0300

    Update script_4.py

--- Task_3/script_4.py
+++ Task_3/script_4.py
@@ -1,13 +1,12 @@
-def decor(f): 
-    def wrapper(n): 
-        list_1 = f(n) 
-        list_ = ' '.join([str(elem) for elem in list_1) 
-        print(list_) 
-    return wrapper 
- 
-@decor 
-def range_l(n):  
-  l = [i for i in range(n+1)]  
-  return l  
-  
-range_list(8)
+def decor_(func):
+    def wrapper(n):
+      m_list = func(n)
+      return list(map(str, m_list))
+    return wrapper
+
+@decor_
+def m_list(n):
+    list_1 = [el for el in range(n + 1)]
+    return list_1
+
+m_list(8)
